0390004f874a632898b1534fc05017bb244937ce
zhuyongan <1215811980@qq.com>
2018-12-23 19:47:15 +0800

    push

---
+++ rep_test/src/com/itheima/test.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("11");
+        System.out.println("11");
+        System.out.println("11");
+        System.out.println("11");
+        System.out.println("11");
+        System.out.println("11");
+    }
+}
